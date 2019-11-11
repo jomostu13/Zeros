0c69888810354c56911cbde9b74c2a7091b5d67e
jomostu13 <54543168+jomostu13@users.noreply.github.com>
2019-11-11 13:49:56 +0700

    Update zeros.js

--- zeros.js
+++ zeros.js
@@ -1,1 +1,19 @@
 function f(x) {
+  return -1*Math.pow(x,3)+7
+}
+
+function df(x) {
+  return -3* Math.pow(x,2)
+}
+
+function newtonsMethod(x, p) {
+  var a = x
+  var b;
+  do {
+    b = a
+    a = a - f(a)/df(a)
+    console.log(a)
+  } while (a-b < p)
+}
+
+
